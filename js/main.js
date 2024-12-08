e3706d06e57a513838162e4e87f23742220d054c
Sanuja Rubasinghe <sanurubasinghe1234@gmail.com>
2024-12-08 18:36:25 +0530

    add responsive design changes

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const menuIcon = document.getElementById("menu-icon")
+const dropmenu = document.querySelector(".dropdown-menu")
+
+menuIcon.addEventListener("click", () => {
+    dropmenu.classList.toggle("hidden")
+    menuIcon.classList.toggle("animate")
+    
+
+})
+
+
+// section reveal animation
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+
+// active page indicator
+const activePage = window.location.pathname
+const navLinks = document.querySelectorAll('nav a')
+navLinks.forEach(link => {
+    if(link.href.includes(`${activePage}`)) {
+        link.classList.add('active')
+    }
+});
+
+
+
+
+window.addEventListener("scroll", reveal, {passive: true});
