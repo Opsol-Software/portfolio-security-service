0f7b24a4dbfaca9c5a010e041c847a0a2ffa575e
DayanHesara <dayanhesara0@gmail.com>
2025-02-16 18:24:05 +0530

    change the leadership styling

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,17 +1,4 @@
-const cards = document.querySelectorAll('.Lcard');
 
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        // Collapse other expanded cards
-        document.querySelectorAll('.Lcard.expanded').forEach(expandedCard => {
-            if (expandedCard !== card) {
-                expandedCard.classList.remove('expanded');
-            }
-        });
-        // Expand the clicked card
-        card.classList.toggle('expanded');
-    });
-});
 
  // JavaScript to handle pop-up modals for certificate images
 
